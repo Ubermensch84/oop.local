9c81129e0ea2825bcb38779b8dcf01c4908f2218
Kirill <llirik_votan@rambler.ru>
2015-08-07 18:30:26 +0300

    Был изменен с добавлением конструктора

--- oop/users.php
+++ oop/users.php
@@ -3,28 +3,27 @@
         public $name;
         public $login;
         public $password;
+        function __construct ($n, $l, $p) {
+            $this -> name = $n;
+            $this -> login = $l;
+            $this -> password = $p;
+        }
+        function __destruct () {
+            echo "<br />Пользователь " . $this -> name . " удален";
+        }
         public function showInfo () {
             echo '<p>Name: ' . $this -> name;
             echo '<br>Login: ' . $this -> login;
             echo '<br>Password: ' . $this -> password;
         }
     }
-$user1 = new User;
-$user1 -> name = 'John Smith';
-$user1 -> login = 'John';
-$user1 -> password = '1234';
+$user1 = new User ('John Smith', 'john', '1234');
 $user1 -> showInfo();
 
-$user2 = new User;
-$user2 -> name = 'Mike Dow';
-$user2 -> login = 'mike';
-$user2 -> password = '4567';
+$user2 = new User ('Mike Dow', 'mike', '4567');
 $user2 -> showInfo();
 
-$user3 = new User;
-$user3 -> name = 'Ivan Petrov';
-$user3 -> login = 'ivan';
-$user3 -> password = '8912';
+$user3 = new User ( 'Ivan Petrov', 'ivan', '8912');
 $user3 -> showInfo();
 
 ?>
