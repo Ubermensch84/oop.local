dd042471c36ba8b0c34c79ffe4bd60314445f326
Kirill <llirik_votan@rambler.ru>
2015-08-07 18:44:14 +0300

    Изменен с добавлением clone

--- oop.php
+++ oop.php
@@ -10,14 +10,23 @@ class Animal {
         echo '<br />';
     }
 
-    function __construct () {
-        echo 'Object created<br>';
+    function __construct ($num) {
+        echo "Object $num created<br>";
+    }
+
+    function __destruct () {
+        echo "Object deleted<br />";
+    }
+
+    function __clone () {
+        echo "Object clone<br />";
     }
 }
 
-$cat = new Animal;
-$dog = new Animal;
+$cat = new Animal(1);
+$dog = new Animal(2);
 $cat->name = 'Мурзик';
+$bigCat = clone $cat;
 $dog->name = 'Тузик';
 $cat->sayHello('Мяу');
 $dog->sayHello('Гав');
